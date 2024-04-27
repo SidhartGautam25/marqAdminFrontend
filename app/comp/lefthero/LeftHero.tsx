f3d9204cd1e4efa72ab995644b671ab6d6f2603f
sidhart25 <navneetnishchal1420@gmail.com>
2024-04-28 03:45:35 +0530

    integration done with few things left

--- app/comp/lefthero/LeftHero.tsx
+++ app/comp/lefthero/LeftHero.tsx
@@ -1,8 +1,9 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useState, useContext } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import axios from "axios";
+import { ReportContext, ReportContextType } from "@/app/context/reportContext";
 
-export default function LeftHero() {
+export default function LeftHero({ reports }) {
   // let [state, setState] = useState({
   //   items: Array.from({ length: 10 }),
   // });
@@ -18,39 +19,48 @@ export default function LeftHero() {
   // };
 
   const [opt, setOpt] = useState<string>("report");
-  const [reports, setReports] = useState([]);
+  //const [reports, setReports] = useState([]);
+  const { state, dispatch } = useContext(ReportContext) as ReportContextType;
   const handleOnChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setOpt(e.target.value);
   };
-  useEffect(() => {
-    // Code inside this function will run after every render
-    // You can perform side effects, such as data fetching, subscriptions, or DOM manipulations here
 
-    // For example, you can fetch data from an API
-    const fetchReport = async () => {
-      console.log("fetch report called");
-      try {
-        const daata = await axios.get(
-          "http://localhost:8800/api/getall/report"
-        );
-        console.log("daata on leftb hero is ", daata);
-        if (daata) {
-          setReports([...daata.data]);
-        }
-      } catch (err) {}
-    };
-    fetchReport();
+  // useEffect(() => {
+  //   // Code inside this function will run after every render
+  //   // You can perform side effects, such as data fetching, subscriptions, or DOM manipulations here
 
-    // You can also return a cleanup function from useEffect
-    // This cleanup function will be executed before the component is unmounted or re-rendered
-    return () => {
-      // Code inside this cleanup function will run before the component is unmounted or re-rendered
-      // You can perform cleanup tasks here, such as unsubscribing from subscriptions or clearing timers
-    };
-  }, []); // The second argument of useEffect is an optional array of dependencies
+  //   // For example, you can fetch data from an API
+  //   const fetchReport = async () => {
+  //     console.log("fetch report called");
+  //     try {
+  //       const daata = await axios.get(
+  //         "http://localhost:8800/api/getall/report"
+  //       );
+  //       console.log("daata on leftb hero is ", daata);
+  //       if (daata) {
+  //         setReports([...daata.data]);
+  //       }
+  //     } catch (err) {}
+  //   };
+  //   fetchReport();
+
+  //   // You can also return a cleanup function from useEffect
+  //   // This cleanup function will be executed before the component is unmounted or re-rendered
+  //   return () => {
+  //     // Code inside this cleanup function will run before the component is unmounted or re-rendered
+  //     // You can perform cleanup tasks here, such as unsubscribing from subscriptions or clearing timers
+  //   };
+  // }, []); // The second argument of useEffect is an optional array of dependencies
 
   // const dataBlag: string[] = ["fgbfgb", "dffdb", "bvfgb", "bfgb", "fbfdb"];
   // const dataReport: string[] = ["dfbfb", "rd", "rd", "rd", "rd"];
+
+  function rclickfun(i) {
+    console.log("obj under rclickfun is ");
+    console.log(i);
+    dispatch({ type: "SET_CURRENT", payload: { iid: i } });
+  }
+
   return (
     <div className="  flex-[1] bg-gray-50 text-xl border-2 border-red-600 rounded-lg py-2">
       <div className="rounded-md flex justify-center">
@@ -108,6 +118,7 @@ export default function LeftHero() {
               <div
                 className=" report w-[350px] flex justify-center  border-[1px] border-gray-600 hover:bg-gradient-to-r from-blue-900 to-black mb-2 p-3 hover:text-white text-center rounded-sm hover:cursor-pointer ease-in duration-200"
                 key={i}
+                onClick={() => rclickfun(i)}
               >
                 {obj.title}
               </div>
--- app/comp/righthero/RightHero.tsx
+++ app/comp/righthero/RightHero.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import Sidebtn from "../sidebtn/Sidebtn";
 import Pdfren from "../pdfren/Pdfren";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { ReportContext, ReportContextType } from "@/app/context/reportContext";
 
-export default function RightHero() {
+export default function RightHero({ reports }) {
+  const { state, dispatch } = useContext(ReportContext) as ReportContextType;
+  console.log("state in right hero is ", state);
+
   return (
     <>
       <div className=" flex-[3]">
@@ -24,7 +28,7 @@ export default function RightHero() {
         <div className="border-2 border-black min-h-[40rem] flex rounded-lg mt-2">
           <div className="border-2 border-black min-h-[40rem] rounded-lg m-2 flex-[1]">
             <div className="border-2 border-black min-h-[40rem] rounded-lg m-2 flex-[3]">
-              <Pdfren />
+              <Pdfren reports={reports} />
             </div>
           </div>
         </div>
