1a5f12b01b88463d5aa25f2d820e5c963241ecde
sidhart25 <navneetnishchal1420@gmail.com>
2024-05-18 15:45:41 +0530

    so many error resolved

--- app/comp/navbar/Navbar.tsx
+++ app/comp/navbar/Navbar.tsx
@@ -1,39 +1,54 @@
 "use client";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext, AuthContextType } from "@/app/context/authContext";
 import { useRouter } from "next/navigation";
 
+const NoSSR: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
 export default function Navbar() {
   const { state, dispatch } = useContext(AuthContext) as AuthContextType;
-  console.log("state us in navbar ", state);
+  // // console.log("state us in navbar ", state);
   const user = state.user;
-  const router = useRouter();
+
   function handlelogout() {
-    localStorage.removeItem("user");
+    // typeof window !== "undefined" ? localStorage.removeItem("user") : null;
     dispatch({ type: "LOGOUT" });
-    router.push("/login");
   }
   return (
-    <nav className="bg-gray-800 text-white p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-bold">
-          <Link href="/">
-            <span className="hover:text-gray-300">Marqstats</span>
-          </Link>
-        </div>
-        <div className="space-x-4">
-          {user ? (
-            <button onClick={handlelogout} className="hover:text-gray-300">
-              Logout
-            </button>
-          ) : (
-            <Link href="/login">
-              <span className="hover:text-gray-300">Login</span>
+    <NoSSR>
+      <nav className="bg-gray-800 text-white p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="text-lg font-bold">
+            <Link href="/">
+              <span className="hover:text-gray-300">Marqstats</span>
             </Link>
-          )}
+          </div>
+          <div className="space-x-4">
+            {user ? (
+              <button onClick={handlelogout} className="hover:text-gray-300">
+                Logout
+              </button>
+            ) : (
+              <Link href="/login">
+                <span className="hover:text-gray-300">Login</span>
+              </Link>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </NoSSR>
   );
 }
