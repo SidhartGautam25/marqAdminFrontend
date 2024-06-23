72f10e67119985d189afc4122e07917046a8f331
sidhart25 <navneetnishchal1420@gmail.com>
2024-06-24 00:13:47 +0530

    after ssl certificate,api url has changed

--- app/utility.ts
+++ app/utility.ts
@@ -1,5 +1,7 @@
 const dev_url="http://localhost:8800"
-const my_admin_url=dev_url
-const prod="https://marq-admin-backend.onrender.com"
+
+const prod1 = "https://marq-admin-backend.onrender.com"
+const prod2="https://api.rahulsinghwebsite.online"
+const my_admin_url=prod2
 
 export {my_admin_url}
